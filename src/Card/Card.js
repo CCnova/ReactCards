fc772c50c15cdbadfdf0145eec44b85ea5eae18a
cnova <caiocnova1@gmail.com>
2019-12-03 07:51:54 -0300

    Add the card component

---
+++ src/Card/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const card = () => {
+    return (
+        <div>
+            <img />
+            <p></p>
+            <button></button>
+        </div>
+    );
+};
+
+export default card;
